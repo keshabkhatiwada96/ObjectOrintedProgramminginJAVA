902856431c08c7aec408ce22ec95328508f23bd0
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-08-09 22:01:35 +0545

    day2 java

---
+++ day2.java
@@ -0,0 +1,14 @@
+class day2 {
+    String name;
+
+    void study() {
+        System.out.println(name + " is studying");
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.name = "CHANDRA SUMSHER JUNG BAHADUR RANA";
+        s1.study();
+    }
+}
