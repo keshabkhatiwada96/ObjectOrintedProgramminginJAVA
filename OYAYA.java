678afd52c5ccd21f95f7e5ab094b7d9003c8d84b
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-09-24 21:48:15 +0545

    try - catch exception handling

---
+++ OYAYA.java
@@ -0,0 +1,12 @@
+public class OYAYA {
+    public static void main(String[] args) {
+        try {
+            int i = 9;
+            int j = i/0;
+            System.err.println(j);
+        } catch (Exception e) {
+
+            System.err.println("eroor occured in try");
+        }
+    }
+}
