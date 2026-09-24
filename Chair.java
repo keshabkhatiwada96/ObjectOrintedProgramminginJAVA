ae8dc2f077c639697805b512a2af17952a32d903
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-09-24 12:44:43 +0545

    java interface implementation

---
+++ Chair.java
@@ -0,0 +1,16 @@
+interface Animal {
+    public void sound();
+}
+
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog making sound bhoww bhoww");
+    }
+}
+
+public class Chair {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.sound();
+    }
+}
