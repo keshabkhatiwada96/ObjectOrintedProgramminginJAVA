3af9730b8996862234ef8a69a3009f909aaf553b
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-09-25 11:49:28 +0545

    try=catch-finally

--- OYAYA.java
+++ OYAYA.java
@@ -1,12 +1,16 @@
-public class OYAYA {
+ class OYAYA {
     public static void main(String[] args) {
         try {
             int i = 9;
             int j = i/0;
             System.err.println(j);
         } catch (Exception e) {
-
+            
             System.err.println("eroor occured in try");
+        }
+        finally {
+               
+            System.out.println("this will run in any conditon either exception or not");
         }
     }
 }
